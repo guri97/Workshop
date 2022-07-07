70c6739e8783d9fb5cd7ec4dde9b38f01cf34c66
guri97 <105732963+guri97@users.noreply.github.com>
2022-07-07 09:33:00 +0530

    [gurpreet] HarmonicNumber

---
+++ Workshop/Workshop/Harmonic.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop
+{
+    internal class Harmonic
+    {
+        public void Harmonic_()
+        {
+            float Harmonic_result = 0;
+            Console.WriteLine("Enter the value of N for Nth harmonic;");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                Harmonic_result += 1 / i;
+            }
+            Console.WriteLine("The Nth harmonic is given by {0}", Harmonic_result);
+        }
+    }
+}
---
+++ Workshop/Workshop/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Workshop
+{
+    class Workshop
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(" Enter the harmonic number");
+            int function = Convert.ToInt32(Console.ReadLine());
+
+            {
+                Harmonic harmonic = new Harmonic();
+                harmonic.Harmonic_();
+            }
+        }
+    }
+}
